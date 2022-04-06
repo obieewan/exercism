876556093fbf416869c37eba79663a14873181db
Obie <imo email sa github>
2022-04-06 23:03:15 +0800

    refactored using if else

--- name-badge/lib/name_badge.ex
+++ name-badge/lib/name_badge.ex
@@ -1,18 +1,39 @@
 defmodule NameBadge do
-  def print(id \\ nil, name, department \\ nil) do
-    # Please implement the print/3 function
-    cond do
-      id == nil and department == nil -> "#{name} - OWNER"
-      
-      department == nil -> "[#{id}] - #{name} - OWNER"
 
-      id == nil -> dept = String.upcase(department)
+
+  def print(id, name, department) do
+    if department == nil do
+      if  id == nil do
+        "#{name} - OWNER"
+      else 
+        "[#{id}] - #{name} - OWNER"
+      end
+    else
+      dept = String.upcase(department)
+      if id == nil do
         "#{name} - #{dept}"
-
-
-      true -> dept = String.upcase(department)
+      else
         "[#{id}] - #{name} - #{dept}"
+      end
     end
   end
+      
+
+
+  #def print(id \\ nil, name, department \\ nil) do
+  #  # Please implement the print/3 function
+  #  cond do
+  #    id == nil and department == nil -> "#{name} - OWNER"
+  #    
+  #    department == nil -> "[#{id}] - #{name} - OWNER"
+
+  #    id == nil -> dept = String.upcase(department)
+  #      "#{name} - #{dept}"
+
+
+  #    true -> dept = String.upcase(department)
+  #      "[#{id}] - #{name} - #{dept}"
+  #  end
+  #end
 
 end
