31456f5de98284e139d7d7386ba7cca0c2a885bc
Obie <imo email sa github>
2022-04-12 23:48:20 +0800

    german to latin char

---
+++ german-sysadmin/lib/username.ex
@@ -0,0 +1,31 @@
+defmodule Username do
+  
+  defguardp is_lower_case(codepoint) when codepoint in ?a..?z 
+  defguardp is_under_score(codepoint) when codepoint == ?_
+  def sanitize(username) do
+    # ä becomes ae
+    # ö becomes oe
+    # ü becomes ue
+    # ß becomes ss
+
+    # Please implement the sanitize/1 function
+    username
+    |> Enum.map(fn char -> german_char_to_latin(char) end) 
+    |> List.flatten
+    |> Enum.filter(fn codepoint -> is_lower_case(codepoint) or is_under_score(codepoint)end)
+  end
+    
+
+  #defguardp is_under_score(codepoint) when codepoint == ?_
+
+  defp german_char_to_latin(char) do
+    case char do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      others -> others
+    end
+  end
+end
+
