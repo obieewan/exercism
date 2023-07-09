3da21f168cd18de6b68652fcef8c75851384a3c0
Obie <obie.tizon@gmail.com>
2023-07-10 01:10:00 +0800

    Conjecture pass

---
+++ collatz-conjecture/lib/collatz_conjecture.ex
@@ -0,0 +1,20 @@
+defmodule CollatzConjecture do
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(1), do: 0
+
+  def calc(input) when input >= 2 do
+    case rem(input, 2) do
+      1 -> 
+        (3 * input) + 1
+
+      0 -> 
+        div(input, 2)
+    end
+  end
+end
